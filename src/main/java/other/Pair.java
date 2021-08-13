76239d812e754dc3a6aa90b440b1f9f48dcbcf85
Barbara Mazzoni <alessia.lombardi4@studio.unibo.it>
2021-08-13 12:34:06 +0200

    Class Pair

--- src/main/java/other/Pair.java
+++ src/main/java/other/Pair.java
@@ -1,65 +1,46 @@
-package a02b.e2;
+package other;
 
-/*
- * A standard generic Pair<X,Y>, with getters, hashCode, equals, and toString well implemented. 
- */
+public class Pair {
 
-public class Pair<X,Y> {
-	
-	private final X x;
-	private final Y y;
-	
-	public Pair(X x, Y y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
+    private double x;
+    private double y;
+    
+    public Pair(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public X getX() {
-		return x;
-	}
+    /**
+     * @return the x
+     */
+    public double getX() {
+        return x;
+    }
 
-	public Y getY() {
-		return y;
-	}
+    /**
+     * @param x the x to set
+     */
+    public void setX(final double x) {
+        this.x = x;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((x == null) ? 0 : x.hashCode());
-		result = prime * result + ((y == null) ? 0 : y.hashCode());
-		return result;
-	}
+    /**
+     * @return the y
+     */
+    public double getY() {
+        return y;
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pair other = (Pair) obj;
-		if (x == null) {
-			if (other.x != null)
-				return false;
-		} else if (!x.equals(other.x))
-			return false;
-		if (y == null) {
-			if (other.y != null)
-				return false;
-		} else if (!y.equals(other.y))
-			return false;
-		return true;
-	}
+    /**
+     * @param y the y to set
+     */
+    public void setY(final double y) {
+        this.y = y;
+    }
 
-	@Override
-	public String toString() {
-		return "Pair [x=" + x + ", y=" + y + "]";
-	}
-	
-	
-
+    @Override
+    public String toString() {
+        return "Pair [x=" + x + ", y=" + y + "]";
+    }
+    
 }
