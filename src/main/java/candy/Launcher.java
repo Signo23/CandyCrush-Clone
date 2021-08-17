469890e72afdcc66d68dc45f32b0894c8154a66c
Lorenzo Signoretti <lory4846@hotmail.it>
2021-08-17 15:09:27 +0200

    Add creation of game's folder

--- src/main/java/candy/Launcher.java
+++ src/main/java/candy/Launcher.java
@@ -1,12 +1,31 @@
 package candy;
 
+import java.io.File;
+
 public final class Launcher {
+
+    /**
+     * User's home directory.
+     */
+    public static final String HOME = System.getProperty("user.home");
+
+    /**
+     * System's file separator.
+     */
+    public static final String SEP = System.getProperty("file.separator");
+
+    /**
+     * Game's main folder.
+     */
+    public static final String MAIN_FOLDER = HOME + SEP + "OOPCandyCrush";
 
     private Launcher() {
     }
 
     public static void main(final String[] args) {
-
+        if (new File(MAIN_FOLDER).mkdirs()) {
+            System.out.println("Directory created succesfully");
+        }
     }
 
 }
