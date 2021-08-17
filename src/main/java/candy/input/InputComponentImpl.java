1a46ffd63e49fafefb471dcdf34b05ec702d49b0
Alessia Lombardi <alessia.lombardi4@studio.unibo.it>
2021-08-17 13:25:00 +0200

    Complete Refill and InputComponentImpl

--- src/main/java/candy/input/InputComponentImpl.java
+++ src/main/java/candy/input/InputComponentImpl.java
@@ -1,14 +1,61 @@
 package candy.input;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import model.game.level.grid.candy.Candy;
 import model.game.level.table.Table;
 import other.Pair;
 
 public class InputComponentImpl implements InputComponent {
 
-    @Override
-    public void update(final Pair position, final InputController c, final Table grid) {
-        // TODO Auto-generated method stub
+    public final void update(final Pair position, final InputController ctrl, final Table grid) {
+            if (ctrl.isSwitchUp()) {
+                final Pair actualPosition = new Pair(position.getX() - 1, position.getY());
+                if (isSwitchPossible(actualPosition, position, grid)
+                    || isSwitchPossible(position, actualPosition, grid)) {
+                    switchCandies(actualPosition, position, grid);
+                }
+            } else if (ctrl.isSwitchDown()) {
+                final Pair actualPosition = new Pair(position.getX() + 1, position.getY());
+                if (isSwitchPossible(actualPosition, position, grid)
+                    || isSwitchPossible(position, actualPosition, grid)) {
+                    switchCandies(actualPosition, position, grid);
+                }
+            } else if (ctrl.isSwitchLeft()) {
+                final Pair actualPosition = new Pair(position.getX(), position.getY() - 1);
+                if (isSwitchPossible(actualPosition, position, grid)
+                    || isSwitchPossible(position, actualPosition, grid)) {
+                    switchCandies(actualPosition, position, grid);
+                }
+            } else if (ctrl.isSwitchRight()) {
+                final Pair actualPosition = new Pair(position.getX(), position.getY() + 1);
+                if (isSwitchPossible(actualPosition, position, grid)
+                    || isSwitchPossible(position, actualPosition, grid)) {
+                    switchCandies(actualPosition, position, grid);
+                }
+            }
+    }
 
+    private boolean isSwitchPossible(final Pair newPos, final Pair oldPos, final Table grid) {
+        final CompleteCheck check = new CompleteCheck();
+        final Pop pop = new PopImpl();
+        final Refill refill = new Refill();
+        final List<Pair> result = check.completeCheck(newPos, oldPos, grid);
+        if (!result.isEmpty()) {
+            pop.pop(result, newPos, grid);
+            refill.scrollDown(grid);
+            return true;
+        }
+        return false;
+    }
+
+    private void switchCandies(final Pair newPos, final Pair oldPos, final Table table) {
+        final Map<Pair, Optional<Candy>> grid = table.getGrid();
+        final Optional<Candy> temp = grid.get(oldPos);
+        grid.put(oldPos, grid.get(newPos));
+        grid.put(newPos, temp);
     }
 
 }
--- src/main/java/candy/input/Refill.java
+++ src/main/java/candy/input/Refill.java
@@ -1,8 +1,36 @@
 package candy.input;
 
-public class Refill {
-    public void scrollDown() {
+import java.util.Map;
+import java.util.Optional;
 
+import model.game.level.grid.candy.Candy;
+import model.game.level.table.Table;
+import model.game.level.table.TableBuilder;
+import model.game.level.table.TableBuilderImpl;
+import other.Pair;
+
+public class Refill {
+    private TableBuilder tableBuilder = new TableBuilderImpl();
+    public final void scrollDown(final Table table) {
+
+        final Map<Pair, Optional<Candy>> grid = table.getGrid();
+        int nRows = grid.getRows();
+        int nColumns = grid.getColumns();
+
+        for (int i = nRows - 1; i > 0; i--) {
+            for (int j = 0; j < nColumns; j++) {
+                final Pair pos = new Pair(i, j);
+                if (grid.get(pos).isEmpty()) { //Posso togliere l'if?
+                    while (grid.get(pos).isEmpty()) {
+                        for (int k = i; k > 0; k--) {
+                            Pair substituteCandy = new Pair(k - 1, j);
+                            grid.put(new Pair(k, j), grid.get(substituteCandy));
+                        }
+                        grid.put(new Pair(0, j), tableBuilder.getRandomNormalCandy(table.getColors()));
+                     }
+                }
+            }
+        }
     }
 
 }
