f96e050d34b4d40bd2b6b6150f90ba17b063792a
Lorenzo Signoretti <lory4846@hotmail.it>
2021-08-18 00:26:23 +0200

    Create graphic's interface

---
+++ src/main/java/candy/graphics/Graphics.java
@@ -0,0 +1,13 @@
+package candy.graphics;
+
+import model.game.level.grid.candy.Candy;
+
+public interface Graphics {
+
+    /**
+     * 
+     * @param candy to draw
+     */
+    void drawCandy(Candy candy);
+
+}
