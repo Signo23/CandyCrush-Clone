148cc9ae67ca1d587ddfa523ddd3d74f8006fee5
Lorenzo Signoretti <lory4846@hotmail.it>
2021-08-17 23:21:28 +0200

    Replacement of method drawGrid with drawCandy

--- src/main/java/candy/graphics/Graphics.java
+++ src/main/java/candy/graphics/Graphics.java
@@ -1,12 +1,9 @@
 package candy.graphics;
 
-import java.util.Map;
-
-import candy.common.Position;
 import model.game.level.grid.candy.Candy;
 
 public interface Graphics {
 
-    void drawGrid(Map<Position, Candy> grid);
+    void drawGrid(Candy candy);
 
 }
