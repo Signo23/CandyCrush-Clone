d38d7e37e7fcfabd0101cc319c4b650f14261fcf
Lorenzo Signoretti <lory4846@hotmail.it>
2021-08-23 13:35:07 +0200

    Create Level.java

--- src/main/java/candy/model/Level.java
+++ src/main/java/candy/model/Level.java
@@ -2,4 +2,42 @@ package candy.model;
 
 public class Level {
 
+    /**
+     * Type of level.
+     * {@value MOVES} level with max number of move for reach the goal's score.
+     * {@value TIME} level with a max time to reach the goal's score,
+     */
+    public enum Type { MOVES, TIME };
+
+    private final Grid grid;
+    private final int scoreGoal;
+    private final Type type;
+
+    public Level(final Grid grid, final int scoreGoal, final Type type) {
+        this.grid = grid;
+        this.scoreGoal = scoreGoal;
+        this.type = type;
+    }
+
+    /**
+     * @return the grid
+     */
+    public Grid getGrid() {
+        return grid;
+    }
+
+    /**
+     * @return the scoreGoal
+     */
+    public int getScoreGoal() {
+        return scoreGoal;
+    }
+
+    /**
+     * @return the type
+     */
+    public Type getType() {
+        return type;
+    }
+
 }
